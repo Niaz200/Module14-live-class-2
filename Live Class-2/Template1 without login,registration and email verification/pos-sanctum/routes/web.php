9106a9ae5ab3d48e7cea368e5b2b4af1a6b3bd98
niazsmart <niaz.smartsoftware@gmail.com>
2024-10-31 11:33:22 +0600

    module14 live class-2

---
+++ Live Class-2/Template1 without login,registration and email verification/pos-sanctum/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+// Page Routes
+Route::view('/','pages.home');
+Route::view('/userLogin','pages.auth.login-page')->name('login');
+Route::view('/userRegistration','pages.auth.registration-page');
+Route::view('/sendOtp','pages.auth.send-otp-page');
+Route::view('/verifyOtp','pages.auth.verify-otp-page');
+Route::view('/resetPassword','pages.auth.reset-pass-page');
+Route::view('/userProfile','pages.dashboard.profile-page');
+
